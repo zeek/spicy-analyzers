84a4a1818cdec83b14ee0d7dde6936514b8659da
Robin Sommer <robin@corelight.com>
2021-02-25 15:33:29 +0000

    Add cmake helpers to track which analyzers are active.

--- cmake/FindSpicy.cmake
+++ cmake/FindSpicy.cmake
@@ -38,6 +38,8 @@ function(run_spicy_config output)
     set(${output} "${output_}" PARENT_SCOPE)
 endfunction ()
 
+# Add target to build analyzer. Arguments are the name of the analyzer and a
+# variable number of source files for `spicyz`.
 function(spicy_add_analyzer name)
     set(sources "${ARGN}")
     set(output "${SPICY_MODULE_OUTPUT_DIR}/${name}.hlto")
@@ -52,7 +54,20 @@ function(spicy_add_analyzer name)
         )
 
     add_custom_target(${name} ALL DEPENDS ${output})
-    # add_dependencies(spicy-analyzers ${name})
+
+    get_property(tmp GLOBAL PROPERTY __spicy_included_analyzers)
+    list(APPEND tmp "${name}")
+    set_property(GLOBAL PROPERTY __spicy_included_analyzers "${tmp}")
+endfunction()
+
+# Flag that analyzer is *not* being built. This is purely informational:
+# the cmake output will contain a corresponding note. Arguments are the
+# name of the analyzers and a descriptive string explaining why it's
+# being skipped.
+function(spicy_skip_analyzer name reason)
+    get_property(tmp GLOBAL PROPERTY __spicy_skipped_analyzers)
+    list(APPEND tmp "${name} ${reason}")
+    set_property(GLOBAL PROPERTY __spicy_skipped_analyzers "${tmp}")
 endfunction()
 
 macro(configure)
@@ -136,19 +151,42 @@ function(print_summary)
                 "\nZeek compiler:         ${SPICYZ} ${SPICYZ_FLAGS}"
                 )
         endif ()
+
     else ()
         message("\n    Make sure spicy-config is in your PATH, or set SPICY_CONFIG to its location.")
     endif ()
 
-    message("\n================================================================\n")
+    message("\n========================================================================\n")
 endfunction ()
+
+function(spicy_print_analyzers)
+    message("\n======================|  Spicy Analyzer Summary  |======================")
+
+    get_property(included GLOBAL PROPERTY __spicy_included_analyzers)
+    message("\nAvailable analyzers:\n")
+    foreach ( x "${included}")
+        message("    ${x}")
+    endforeach ()
+
+    get_property(skipped GLOBAL PROPERTY __spicy_skipped_analyzers)
+    if ( skipped )
+        message("\nSkipped analyzers:\n")
+        foreach ( x ${skipped})
+            message("    ${x}")
+        endforeach ()
+    endif ()
+
+    message("\n========================================================================\n")
+endfunction()
 
 ### Main
 
 option(SPICY_IN_TREE_BUILD "Internal option to flag building from within the Spicy source tree" no)
 
-# add_custom_target(spicy-analyzers ALL)
 set(SPICY_MODULE_OUTPUT_DIR "${PROJECT_BINARY_DIR}/spicy-modules")
+
+set_property(GLOBAL PROPERTY __spicy_included_analyzers)
+set_property(GLOBAL PROPERTY __spicy_skipped_analyzers)
 
 if ( "${SPICY_IN_TREE_BUILD}" )
 else ()
